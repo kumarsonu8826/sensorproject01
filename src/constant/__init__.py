b9e3bb90bd8ead4f018e17545075e075faa2c5af
sonu kumar <sonukumar.in065@gmail.com>
2025-03-20 19:48:09 +0530

    added moduls

---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+
+AWS_S3_BUCKET_NAME = 'water_fault'
+MANGO_DATABASE_NAME = "pwskills"
+MANGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLLUMN= "quality"
+MANGO_DB_URL = "mongodb+srv://snshrivas:Snshrivas@cluster0.u6c4.mongodb.net/?retryWrites=true&w=majority"
+
+
+MODEL_FILE_NAME= 'model'
+MODEL_FILE_EXTENSION= ".pkl"
